b989b528057ccc7bfbeda31898f87958b270507f
atippaz <nasakun13201@gmail.com>
2023-10-31 15:54:06 +0700

    add pwa

---
+++ vue.config.ts
@@ -0,0 +1,12 @@
+export default {
+    pwa: {
+        // ...Other pwa config options
+        icons: {
+            favicon32: './logo.ico',
+            favicon16: './logo.ico',
+            appleTouchIcon: './logo.ico',
+            maskIcon: './logo.ico',
+            msTileImage: './logo.ico',
+        },
+    },
+}
