21272bea2e0ed99e5522b05b2542f5a7c81fb6af
atippaz <nasakun13201@gmail.com>
2023-10-31 16:06:19 +0700

    ียกฟะำ ยไฟ

--- src/main.ts
+++ src/main.ts
@@ -19,4 +19,18 @@ const app = createApp(App)
 
 registerPlugins(app)
 
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+    // Register the service worker
+    navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => {
+            console.log(
+                'Service Worker registered with scope:',
+                registration.scope
+            )
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error)
+        })
+}
 app.mount('#app')
